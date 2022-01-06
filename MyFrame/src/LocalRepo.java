afa148d73ba4e2c413af6ccfe15db54d2112f31a
Seyar <seyarjoyandah.sj@gmail.com>
2022-01-05 20:16:20 -0500

    this is array test

--- MyFrame/src/LocalRepo.java
+++ MyFrame/src/LocalRepo.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
 
 public class LocalRepo {
 
 	public static void main(String[] args) {
 	
 		System.out.println("This is test");
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for(int i = 1; i<=10; i++) {
+			arr.add(i);
+		}
+		System.out.println(arr);
 
 	}
 
